2043e5a22487fae9a9e67db49a00087989bcfb69
Francisco Mendoza González <alu0100600582@ull.edu.es>
2016-01-02 20:34:39 +0100

    añadiendo load al controlador

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,19 +1,31 @@
 var models = require('../models/models');
-
 var quiz = models.Quiz;
 
+
+exports.load = function(req, res, next, quizId) {
+  Quiz.find(quizId).then(
+      function(quiz) {
+        if (quiz) {
+          req.quiz = quiz;
+          next();
+        } else { next(new Error('No existe quizId = '+quizId)); }
+      }
+      ).catch( function(error) { next(error); });
+};
+
+
 exports.show = function(req,res) {
-  res.render('quizes/show', {quiz: quiz});
+  res.render('quizes/show', {quiz: req.quiz});
 };
 
 exports.answer = function(req, res) {
-  var respuesta = 'Incorrecto';
-  if (req.query.respuesta === req.quiz.respuesta) respuesta = 'Correcto';
-  res.render('quizes/answer', { quiz: req.quiz, respuesta: respuesta })
+  var resultado = 'Incorrecto';
+  if (req.query.respuesta === req.quiz.respuesta) resultado = 'Correcto';
+  res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado })
 };
 
-exports.index = function(req, res){
+exports.index = function(req, res) {
   models.Quiz.findAll().then(function(quizes){
-    res.render('quizes/index.ejs', {quizes: quizes});
-  })
+    res.render('quizes/index', { quizes: quizes});
+  }).catch(function(error) { next(error); });
 };
