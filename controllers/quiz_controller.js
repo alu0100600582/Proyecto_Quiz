63c899c5067540e4e50bbbd97dc49ce5f4593baf
Francisco Mendoza González <alu0100600582@ull.edu.es>
2016-01-03 19:34:18 +0100

    Ya funciona la validacion

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -38,18 +38,14 @@ exports.new = function(req,res){
 
 
  exports.create =function(req,res){
-   var quiz = models.Quiz.build(req.body.quiz);
+   var quiz = Quiz.build(req.body.quiz);
 
-   quiz
-   .validate()
-   .then(function(err){
+   quiz.validate().then(function(err){
      if(err){
        res.render('quizes/new', {quiz: quiz, errors: err.errors});
      } else {
-         quiz
-         .save({fields: ['pregunta', 'respuesta']})
-         .then(function(){res.redirect('/quizes')})
-       }
+         quiz.save({fields: ['pregunta', 'respuesta']}).then(function(){
+         res.redirect('/quizes')})
      }
-        );
-  };
+   });
+ };
