d48f3f14b1d88633d1b891ec3573e819fadada34
Francisco Mendoza González <alu0100600582@ull.edu.es>
2016-01-03 20:57:00 +0100

    modificacion de rutas para requerir autenticacion

--- routes/index.js
+++ routes/index.js
@@ -16,11 +16,11 @@ router.param('quizId', quizController.load);
 router.get('/quizes',                           quizController.index);
 router.get('/quizes/:quizId(\\d+)',             quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',      quizController.answer);
-router.get('/quizes/new',                       quizController.new);
-router.post('/quizes/create',                   quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit',        quizController.edit);
-router.put('/quizes/:quizId(\\d+)',             quizController.update);
-router.delete('/quizes/:quizId(\\d+)',          quizController.destroy);
+router.get('/quizes/new',                       sessionController.loggingRequired, quizController.new);
+router.post('/quizes/create',                   sessionController.loggingRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',        sessionController.loggingRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)',             sessionController.loggingRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)',          sessionController.loggingRequired, quizController.destroy);
 
 router.get('/quizes/:quizId(\\d+)/comments/new',    commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',       commentController.create);
