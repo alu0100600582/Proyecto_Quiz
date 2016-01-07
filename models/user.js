d1063fb7aa4afe8d836a89df0c6e5bdf249ccbdc
Francisco Mendoza González <alu0100600582@ull.edu.es>
2016-01-07 22:48:03 +0100

    :-1:  :-1:

--- models/user.js
+++ models/user.js
@@ -35,9 +35,7 @@ module.exports = function(sequelize, DataTypes) {
                 type: DataTypes.BOOLEAN,
                 defaultValue: false
             }
-        },
-
-    );
+        });
 
     return User;
 }
